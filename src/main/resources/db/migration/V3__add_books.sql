66246c5f73b5b34f5351091b85795acf35ee202a
AndruXxX <saldj.andre@gmail.com>
2022-10-14 20:53:13 +0300

    Feature Get Books

--- src/main/resources/db/migration/V3__add_books.sql
+++ src/main/resources/db/migration/V3__add_books.sql
@@ -3,6 +3,28 @@ VALUES (1, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156
        (2, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
        (3, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
        (4, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
-       (5, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null);
+       (5, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (6, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (7, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (8, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (9, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (10, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (11, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (12, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (13, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (14, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (15, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (16, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (17, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (18, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (19, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (20, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (21, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (22, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (23, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (24, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (25, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (26, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null),
+       (27, 'sadfrwas', 'asdfgsadf', '/Photos/Latin_dictionary.jpg', 'Aboba', 156, null, null);
 
-ALTER SEQUENCE book_seq RESTART WITH 6;
+ALTER SEQUENCE book_seq RESTART WITH 28;
