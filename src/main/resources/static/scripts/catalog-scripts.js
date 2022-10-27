1e6f4f672447220897c2848b650169d7d57a0485
LostD1r <rodina.mark2015@gmail.com>
2022-10-27 22:51:14 +0300

    Added pushing search parameters to URL

--- src/main/resources/static/scripts/catalog-scripts.js
+++ src/main/resources/static/scripts/catalog-scripts.js
@@ -57,3 +57,10 @@ function showCheckboxesAuthors() {
         expanded = false;
     }
 }
+
+function makeSearch() {
+    let url = new URL(document.location.href);
+    let param = document.getElementById('search').value;
+    url.searchParams.append('search', param);
+    window.location.href = url.origin + url.pathname + url.search;
+}
