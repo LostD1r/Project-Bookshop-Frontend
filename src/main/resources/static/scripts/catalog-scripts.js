e0c3a1246f5b1aa2dae1858e0b39e6b0962e9d47
LostD1r <rodina.mark2015@gmail.com>
2022-10-28 14:47:33 +0300

    Fixed local database

--- src/main/resources/static/scripts/catalog-scripts.js
+++ src/main/resources/static/scripts/catalog-scripts.js
@@ -1,20 +1,3 @@
-// function dropdownOpen(){
-//     document.getElementById("dropdown-items").classList.add("show");
-// }
-//
-// window.onclick = function (event){
-//     if(!event.target.matches('.dropdown-main-button')){
-//         let dropdowns = document.getElementsByClassName("dropdown-content");
-//         let i;
-//         for(i = 0; i < dropdowns.length; i++){
-//             let openDropdown = dropdowns[i];
-//             if(openDropdown.classList.contains("show")){
-//                 openDropdown.classList.remove("show");
-//             }
-//         }
-//     }
-// }
-
 let expanded = false;
 
 function showCheckboxesGenres() {
