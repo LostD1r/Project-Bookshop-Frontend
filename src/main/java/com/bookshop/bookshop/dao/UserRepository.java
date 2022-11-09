046d42aa6320ea85a9f88a2c9775d43371a8f496
AndruXxX <saldj.andre@gmail.com>
2022-11-09 02:21:09 +0200

    Added person detail validation.

--- src/main/java/com/bookshop/bookshop/dao/UserRepository.java
+++ src/main/java/com/bookshop/bookshop/dao/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByName(String name);
     User findById(long id);
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/bookshop/bookshop/dto/UserDTO.java
+++ src/main/java/com/bookshop/bookshop/dto/UserDTO.java
@@ -5,14 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserDTO {
+
+    @NotBlank(message = "Будь ласка придумайте свій нікнейм.")
+    @Size(min = 2, max = 20, message = "Нікнейм повинен містити від 2 до 20 символів.")
     private String username;
+
+    @NotBlank(message = "Будь ласка придумайте важкий пароль.")
+    @Size(min = 8, max = 30, message = "Пароль повинен бути від 8 до 30 символів.")
     private String password;
     private String matchingPassword;
+
+    @NotBlank(message = "Будь ласка введіть свою елктронну адресу.")
+    @Email(message = "Елктронна адреса повинна мати таку структуру \"test@test.com\"")
     private String email;
 }
 
