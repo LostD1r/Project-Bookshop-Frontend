6d78cbbec0d2156846fd66c8e869b792ca2973e9
AndruXxX <saldj.andre@gmail.com>
2022-11-12 01:08:45 +0200

    Added user CRUD ability

---
+++ src/main/java/com/bookshop/bookshop/dto/ChangeUserRoleDto.java
@@ -0,0 +1,14 @@
+package com.bookshop.bookshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangeUserRoleDto {
+    private String role;
+}
