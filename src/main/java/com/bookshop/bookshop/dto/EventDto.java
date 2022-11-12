323ff246e174e5393923ac7ac85b1633b401fb7f
AndruXxX <saldj.andre@gmail.com>
2022-11-12 04:23:59 +0200

    Added Event CRUD abilities

---
+++ src/main/java/com/bookshop/bookshop/dto/EventDto.java
@@ -0,0 +1,15 @@
+package com.bookshop.bookshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EventDto {
+    private String title;
+    private String message;
+}
