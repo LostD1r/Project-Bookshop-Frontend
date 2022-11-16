4f3c35afc849ff141769f13fbff79e3fd07a97a3
DESKTOP-9DN30IL\mline <mlineckiyvitaliy@gmail.com>
2022-11-16 13:18:19 +0200

    Added user change password ability and other micro-fix

--- src/main/java/com/bookshop/bookshop/controllers/UserController.java
+++ src/main/java/com/bookshop/bookshop/controllers/UserController.java
@@ -1,24 +1,32 @@
 package com.bookshop.bookshop.controllers;
 
+import com.bookshop.bookshop.dto.PasswordDto;
 import com.bookshop.bookshop.models.Book;
 import com.bookshop.bookshop.models.User;
 import com.bookshop.bookshop.security.PersonDetails;
 import com.bookshop.bookshop.service.PersonDetailsService;
+import com.bookshop.bookshop.service.RegistrationService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 
 @Controller
 public class UserController {
     private final PersonDetailsService personDetailsService;
+    private final RegistrationService registrationService;
 
-    public UserController(PersonDetailsService personDetailsService) {
+    public UserController(PersonDetailsService personDetailsService, RegistrationService registrationService) {
         this.personDetailsService = personDetailsService;
+        this.registrationService = registrationService;
     }
 
     @GetMapping("/profile")
@@ -40,6 +48,22 @@ public class UserController {
 
     @GetMapping("/profile/settings")
     public String settingProfile(Model model){
+        model.addAttribute("password", new PasswordDto());
         return "user-page-settings";
+    }
+
+    @PostMapping("/profile/settings/password")
+    public String changePassword(@Valid @ModelAttribute PasswordDto passwordDto, BindingResult bindingResult, Model model){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+
+        if(!registrationService.changePassword(personDetails.getUsername(), passwordDto, bindingResult)){
+            return "user-page-settings";
+        }
+        if(bindingResult.hasErrors()){
+            return "user-page-settings";
+        }
+        return "redirect:/profile";
     }
 }
